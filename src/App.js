23d0c07feb608e554d038b8f1713fee65ec2d716
JohnPCarvalho <johnnypcarvalho@gmail.com>
2021-04-07 21:30:36 -0300

    first commit of the app - basic structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <h1>The King of the Burger apps</h1>
+      </div>
+    );
+  }
+}
+
+export default App;

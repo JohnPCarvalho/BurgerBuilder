417bb30688eb7bbed2cd060dbdc3a112a4e47c51
JohnPCarvalho <johnnypcarvalho@gmail.com>
2021-04-09 23:16:04 -0300

    adding the build control component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -13,7 +13,8 @@ const burger = ( props ) => {
     }).reduce((arr, el) => {
       return arr.concat(el);
     }, []);
-
+    console.log("Final: ");
+    console.log(transformedIngredients);
     if (transformedIngredients.length === 0) {
       transformedIngredients = <p>Please start adding ingredients</p>
     }
